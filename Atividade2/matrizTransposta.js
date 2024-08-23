3078c82294d6987656fe8940b673b8583cab00aa
diegombrito04 <mazurokdiego@gmail.com>
2024-08-23 19:52:32 -0300

    Update matrizTransposta.js

--- Atividade2/matrizTransposta.js
+++ Atividade2/matrizTransposta.js
@@ -1,1 +1,33 @@
+function transporMatriz(A) {
+    let linhas = A.length;
+    let colunas = A[0].length;
+    
+    
+    let matrizTransposta = [];
+    for (let i = 0; i < colunas; i++) {
+        matrizTransposta[i] = [];
+        for (let j = 0; j < linhas; j++) {
+            matrizTransposta[i][j] = A[j][i];
+        }
+    }
+
+    console.log("Matriz Original:");
+    for (let i = 0; i < linhas; i++) {
+        console.log(A[i]);
+    }
+
+    console.log("\nMatriz Transposta:");
+    for (let i = 0; i < colunas; i++) {
+        console.log(matrizTransposta[i]);
+    }
+}
+
+// Um exemplo do uso:
+let matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+transporMatriz(matriz);
 
