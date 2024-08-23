eb4272973bbda7b9317c2f81af9f237ba9c0e087
diegombrito04 <mazurokdiego@gmail.com>
2024-08-23 19:46:29 -0300

    Update variaveisOperadores.js
    Adicionei o código

--- Atividade1/variaveisOperadores.js
+++ Atividade1/variaveisOperadores.js
@@ -1,1 +1,20 @@
+let numA1 = 5;
+let numB1 = 10;
+let resultado1 = numA1 + numB1;
+console.log(`Resultado da adição entre ${numA1} e ${numB1} é ${resultado1}`);
+
+let numA2 = 20;
+let numB2 = 8;
+let resultado2 = numA2 - numB2;
+console.log(`Resultado da subtração entre ${numA2} e ${numB2} é ${resultado2}`);
+
+let numA3 = 7;
+let numB3 = 3;
+let resultado3 = numA3 * numB3;
+console.log(`Resultado da multiplicação entre ${numA3} e ${numB3} é ${resultado3}`);
+
+let numA4 = 16;
+let numB4 = 4;
+let resultado4 = numA4 / numB4;
+console.log(`Resultado da divisão entre ${numA4} e ${numB4} é ${resultado4}`);
 
